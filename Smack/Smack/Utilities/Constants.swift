345061e9e38264fceb5e2a1fafef6e6e1da336a5
ths <ths@Thars-MacBook-Air.local>
2018-01-13 16:47:42 +0630

    lesson-06 finished

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,7 +8,22 @@
 
 import Foundation
 
+typealias ComplectionHandle = (_ Success : Bool )->()
+
+//URL Constant
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 //MARK: Segues
 let TO_LOG_IN = "toLogin_segue"
 let TO_CREATE_ACCOUNT = "toCreateAccount_segue"
 let UNWIN = "unwinToChannel"
+
+
+
+//MARK:user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedin"
+let USER_EMAIL  = "userEmail"
+
